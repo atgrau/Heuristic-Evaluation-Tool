0b51a2be7c0aef2a94fafc81f8d74453b5e64db4
Albert <agrau.85@gmail.com>
2018-05-21 17:54:38 +0200

    - Added Template Controller - Added Template Model

---
+++ src/controller/TemplateController.class.php
@@ -0,0 +1,12 @@
+<?php
+  require_once(BASE_URI."model/TemplateModel.class.php");
+
+  /**
+   * Template Controller
+   */
+  class TemplateController extends BaseController
+  {
+    function __construct() { }
+  }
+
+?>
---
+++ src/model/TemplateModel.class.php
@@ -0,0 +1,48 @@
+<?php
+  /**
+   * Questions
+   */
+  class Question
+  {
+
+    private $id;
+    private $order;
+    private $question;
+
+    function __construct($id, $order, $question)
+    {
+      $this->id = $id;
+      $this->order = $order;
+      $this->question = $question;
+    }
+
+    function getId() {
+      return $this->id;
+    }
+
+    function getOrder() {
+      return $this->order;
+    }
+
+    function setOrder($value) {
+      $this->order = $value;
+    }
+
+    function getQuestion() {
+      return $this->questions;
+    }
+
+    function setQuestion($value) {
+      $this->question = $value;
+    }
+
+    function getQuestions() {
+
+    }
+
+    function getQuestionById($id) {
+
+    }
+  }
+
+?>
