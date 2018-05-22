42be3ba4e472842fae49b8d51bcc3120c43b689c
Albert <agrau.85@gmail.com>
2018-05-22 10:38:59 +0200

    Config

--- src/config/global.php
+++ src/config/global.php
@@ -6,5 +6,12 @@
   define("APP_TITLE", "Heuristic Evaluation Tool");
 
   // Email Configuration
-
+  define("SMTP_SERVER", "smtp.gmail.com");
+  define("SMTP_PORT", 587);
+  define("SMTP_USER", "**");
+  define("SMTP_PASSWORD", "**");
+  define("SMTP_SECURE", "tls");
+  define("SMTP_AUTH", true);
+  define("SMTP_NAME", "Heuristic Evaluation Tool");
+  define("SMTP_FROM", "**");
 ?>
