01873c27ff751bbe372a0c75609321f78a6b6bc0
Albert <agrau.85@gmail.com>
2018-06-26 16:11:56 +0200

    GraphsJS on Evaluation

--- src/view/evaluation/template.php
+++ src/view/evaluation/template.php
@@ -1,39 +1,38 @@
 <button id="topButton" title="Go to top"><span class="glyphicon glyphicon-menu-up"></span></button>
-<div class="row">
-    <div class="col-lg-12">
-      <h1 class="page-header">Evaluation of: <small><?=$this->evaluation->getProject()->getName();?></small></h1>
-    </div>
-    <!-- /.col-lg-12 -->
-</div>
-<!-- /.row -->
-<!-- /.row -->
-<div class="row">
-  <table class="table">
-    <thead>
-    </thead>
-    <tbody>
-      <th colspan="2" class="thead-light text-center">Project Information</th>
-      <tr>
-        <th width="20%">Project's Name:</th>
-        <td><?=$this->evaluation->getProject()->getName();?></td>
-      </tr>
-      <tr>
-        <th>Project's Description:</th>
-        <td><?=$this->evaluation->getProject()->getDescription();?></td>
-      </tr>
-      <tr>
-        <th>Project's Link:</th>
-        <td><a href="<?=$this->evaluation->getProject()->getLink();?>" target="_blank" title="Link to <?=$this->evaluation->getProject()->getLink();?>"><?=$this->evaluation->getProject()->getLink();?></a></td>
-      </tr>
-      <tr>
-        <th>Ending Date:</th>
-        <td><?=$this->evaluation->getProject()->getFinishDate();?></td>
-      </tr>
-    </tbody>
-  </table>
-  <form id="evaluation_form">
+  <div class="row">
+      <div class="col-lg-12">
+        <h1 class="page-header">Evaluation of: <small><?=$this->evaluation->getProject()->getName();?></small></h1>
+      </div>
+      <!-- /.col-lg-12 -->
+  </div>
+  <!-- /.row -->
+  <!-- /.row -->
+  <div class="row">
+    <table class="table">
+      <thead>
+      </thead>
+      <tbody>
+        <th colspan="2" class="thead-light text-center">Project Information</th>
+        <tr>
+          <th width="20%">Project's Name:</th>
+          <td><?=$this->evaluation->getProject()->getName();?></td>
+        </tr>
+        <tr>
+          <th>Project's Description:</th>
+          <td><?=$this->evaluation->getProject()->getDescription();?></td>
+        </tr>
+        <tr>
+          <th>Project's Link:</th>
+          <td><a href="<?=$this->evaluation->getProject()->getLink();?>" target="_blank" title="Link to <?=$this->evaluation->getProject()->getLink();?>"><?=$this->evaluation->getProject()->getLink();?></a></td>
+        </tr>
+        <tr>
+          <th>Ending Date:</th>
+          <td><?=$this->evaluation->getProject()->getFinishDate();?></td>
+        </tr>
+      </tbody>
+    </table>
+
     <div id="result" style="display:none"></div>
-    <input name="id_evaluation" type="hidden" value="<?=$this->evaluation->getId();?>">
     <div class="col-lg">
         <!-- /.panel-heading -->
         <div class="panel-body">
@@ -58,37 +57,38 @@
           <div class="tab-content">
 
             <div class="tab-pane fade in active margin-lg-t" id="template">
-
-              <table class="table">
-                <thead class="thead-light">
-                  <tr>
-                    <th colspan="2">Categoría 1</th>
-                    <th>Answer</th>
-                    <th>Comments</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row" width="20px">#1</th>
-                    <td width="50%">Pregunta nº 1</td>
-                    <td width="20%">
-                      <div class="form-group">
-                        <select name="role" class="form-control">
-                          <option value="0">Evaluator</option>
-                          <option value="1">Project Manager</option>
-                          <option value="2">Administrator</option>
-                        </select>
-                       </div>
-                    </td>
-                    <td width="30%">
-                      <div class="form-group">
-                        <textarea class="form-control"></textarea>
-                       </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-
+              <form id="evaluation_form">
+                <input name="id_evaluation" type="hidden" value="<?=$this->evaluation->getId();?>">
+                <table class="table">
+                  <thead class="thead-light">
+                    <tr>
+                      <th colspan="2">Categoría 1</th>
+                      <th>Answer</th>
+                      <th>Comments</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row" width="20px">#1</th>
+                      <td width="50%">Pregunta nº 1</td>
+                      <td width="20%">
+                        <div class="form-group">
+                          <select name="role" class="form-control">
+                            <option value="0">Evaluator</option>
+                            <option value="1">Project Manager</option>
+                            <option value="2">Administrator</option>
+                          </select>
+                         </div>
+                      </td>
+                      <td width="30%">
+                        <div class="form-group">
+                          <textarea class="form-control"></textarea>
+                         </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </form>
             </div>
 
             <div class="tab-pane fade in margin-lg-t" id="project" >
@@ -123,13 +123,12 @@
               <div class="col-lg-6">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Pie Chart Example
+                        Answer Chart
                     </div>
                     <!-- /.panel-heading -->
                     <div class="panel-body">
-                        <div class="flot-chart">
-                            <div class="flot-chart-content" id="flot-pie-chart" style="padding: 0px; position: relative;"><canvas class="flot-base" width="472" height="400" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 472px; height: 400px;"></canvas><canvas class="flot-overlay" width="472" height="400" style="direction: ltr; position: absolute; left: 0px; top: 0px; width: 472px; height: 400px;"></canvas><div class="legend"><div style="position: absolute; width: 57px; height: 64px; top: 5px; right: 5px; background-color: rgb(255, 255, 255); opacity: 0.85;"> </div><table style="position:absolute;top:5px;right:5px;;font-size:smaller;color:#545454"><tbody><tr><td class="legendColorBox"><div style="border:1px solid #ccc;padding:1px"><div style="width:4px;height:0;border:5px solid rgb(237,194,64);overflow:hidden"></div></div></td><td class="legendLabel">Series 0</td></tr><tr><td class="legendColorBox"><div style="border:1px solid #ccc;padding:1px"><div style="width:4px;height:0;border:5px solid rgb(175,216,248);overflow:hidden"></div></div></td><td class="legendLabel">Series 1</td></tr><tr><td class="legendColorBox"><div style="border:1px solid #ccc;padding:1px"><div style="width:4px;height:0;border:5px solid rgb(203,75,75);overflow:hidden"></div></div></td><td class="legendLabel">Series 2</td></tr><tr><td class="legendColorBox"><div style="border:1px solid #ccc;padding:1px"><div style="width:4px;height:0;border:5px solid rgb(77,167,77);overflow:hidden"></div></div></td><td class="legendLabel">Series 3</td></tr></tbody></table></div></div>
-                        </div>
+                      <canvas id="chartjs-1" class="chartjs" width="770" height="385" style="display: block; width: 770px; height: 385px;"></canvas>
+                      <script>new Chart(document.getElementById("chartjs-1"),{"type":"doughnut","data":{"labels":["Red","Blue","Yellow"],"datasets":[{"label":"My First Dataset","data":[300,50,100],"backgroundColor":["rgb(255, 99, 132)","rgb(54, 162, 235)","rgb(255, 205, 86)"]}]}});</script>
                     </div>
                     <!-- /.panel-body -->
                 </div>
@@ -138,79 +137,70 @@
               <div class="col-lg-6">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            Pie Chart Example
+                            Answer Chart
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
-                          <canvas id="myChart" width="400" height="400"></canvas>
-<script>
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-      }]
-  },
-  options: {
-      scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero:true
-              }
-          }]
-      }
-  }
-});
-</script>
+                          <canvas id="chartjs-2" class="chartjs" width="770" height="385" style="display: block; width: 770px; height: 385px;"></canvas>
+                          <script>new Chart(document.getElementById("chartjs-2"),{"type":"doughnut","data":{"labels":["Red","Blue","Yellow"],"datasets":[{"label":"My First Dataset","data":[300,50,100],"backgroundColor":["rgb(255, 99, 132)","rgb(54, 162, 235)","rgb(255, 205, 86)"]}]}});</script>
                         </div>
                         <!-- /.panel-body -->
                     </div>
                     <!-- /.panel -->
                 </div>
 
-              <table class="table">
-                <thead>
-                </thead>
-                <tbody>
-                  <th colspan="2" class="thead-light text-center">Global Results</th>
-                  <tr>
-                    <th width="20%">Total Evaluations:</th>
-                    <td><?=$this->evaluation->getProject()->getName();?></td>
-                  </tr>
-                  <tr>
-                    <th>Project's Description:</th>
-                    <td><?=$this->evaluation->getProject()->getDescription();?></td>
-                  </tr>
-                  <tr>
-                    <th>Project's Link:</th>
-                    <td><a href="<?=$this->evaluation->getProject()->getLink();?>" target="_blank" title="Link to <?=$this->evaluation->getProject()->getLink();?>"><?=$this->evaluation->getProject()->getLink();?></a></td>
-                  </tr>
-                  <tr>
-                    <th>Ending Date:</th>
-                    <td><?=$this->evaluation->getProject()->getFinishDate();?></td>
-                  </tr>
-                </tbody>
-              </table>
+                <table class="table">
+                  <thead>
+                  </thead>
+                  <tbody>
+                    <th colspan="3" class="thead-light text-center">Global results</th>
+                    <tr>
+                      <th width="20%">Total Questions:</th>
+                      <td width="20%">80</td>
+                    </tr>
+                    <tr>
+                      <th>Total answered Questions:</th>
+                      <td>75</td>
+                    </tr>
+                    <tr>
+                      <th>Score</th>
+                      <td>65</td>
+                    </tr>
+                    <tr>
+                      <th>Finished at:</th>
+                      <td><?=$this->evaluation->getProject()->getFinishDate();?></td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                <div class="col-lg-6">
+                  <div class="panel panel-default">
+                      <div class="panel-heading">
+                          Answer Chart
+                      </div>
+                      <!-- /.panel-heading -->
+                      <div class="panel-body">
+                        <canvas id="chartjs-3" class="chartjs" width="770" height="385" style="display: block; width: 770px; height: 385px;"></canvas>
+                        <script>new Chart(document.getElementById("chartjs-3"),{"type":"doughnut","data":{"labels":["Red","Blue","Yellow"],"datasets":[{"label":"My First Dataset","data":[300,50,100],"backgroundColor":["rgb(255, 99, 132)","rgb(54, 162, 235)","rgb(255, 205, 86)"]}]}});</script>
+                      </div>
+                      <!-- /.panel-body -->
+                  </div>
+                  <!-- /.panel -->
+                </div>
+                <div class="col-lg-6">
+                      <div class="panel panel-default">
+                          <div class="panel-heading">
+                              Answer Chart
+                          </div>
+                          <!-- /.panel-heading -->
+                          <div class="panel-body">
+                            <canvas id="chartjs-4" class="chartjs" width="770" height="385" style="display: block; width: 770px; height: 385px;"></canvas>
+                            <script>new Chart(document.getElementById("chartjs-4"),{"type":"doughnut","data":{"labels":["Red","Blue","Yellow"],"datasets":[{"label":"My First Dataset","data":[300,50,100],"backgroundColor":["rgb(255, 99, 132)","rgb(54, 162, 235)","rgb(255, 205, 86)"]}]}});</script>
+                          </div>
+                          <!-- /.panel-body -->
+                      </div>
+                      <!-- /.panel -->
+                  </div>
             </div>
           </div>
         </div>
