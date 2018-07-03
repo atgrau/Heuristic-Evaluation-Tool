a0fb8733dbbdce4f682e549f907de066a73389a2
jesoch <jesoch@gmail.com>
2018-07-03 13:14:52 +0200

    Bugs template

--- src/view/template/template.php
+++ src/view/template/template.php
@@ -1,32 +1,7 @@
 <button id="topButton" title="Go to top"><span class="glyphicon glyphicon-menu-up"></span></button>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-colorpicker/2.3.3/css/bootstrap-colorpicker.min.css" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-colorpicker/2.3.3/js/bootstrap-colorpicker.min.js"></script>
-<?php
-function generateModal($categoryId) {
-return '<!-- Modal -->
-<div class="modal fade" id="deletingModal_'.$categoryId.'" tabindex="-1" role="dialog" aria-labelledby="deletingModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <strong class="text-danger">Category Deletion</strong>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <h4>Category #'.$categoryId.' and all it related questions <span class="bg-danger">will be removed too</span>.<br /><br />Are you sure?</h4>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <a name="" href="/admin/remove-category/'.$categoryId.'" type="button" class="btn btn-danger">Delete User</a>
-      </div>
-    </div>
-  </div>
-</div>';
-}
 
-
-?>
 <div class="row">
 
   <div class="row">
