3871775c88cf5d067429d0dc2e67aac29f0bfc6b
Albert <agrau.85@gmail.com>
2018-07-05 13:57:49 +0200

    Content update

--- src/view/privacy-policy.php
+++ src/view/privacy-policy.php
@@ -6,11 +6,11 @@
     <p>Effective date: July 05, 2018</p>
 
 
-    <p><?=APP_TITLE;?> ("us", "we", or "our") operates the URI website (the "Service").</p>
+    <p><?=APP_TITLE;?> ("us", "we", or "our") operates the <a href="/"><?=URL?></a> website (the "Service").</p>
 
     <p>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data.</p>
 
-    <p>We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from URI</p>
+    <p>We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy.</p>
 
 
     <h2>Information Collection And Use</h2>
