679b61ed3f5758d8df6cdb883bd140255477f250
Roberto García <roberto@rhizomik.net>
2018-12-22 11:09:07 +0100

    Load MySQL config vars from environment

--- src/config/database.php
+++ src/config/database.php
@@ -1,9 +1,11 @@
 <?php
   # MySQL Configuration
   define("MYSQL_ENCODING", "utf8");
-  define("MYSQL_USER", "root");
-  define("MYSQL_PWD", "toor");
-  define("MYSQL_DB", "heuristic_evaluation");
+  define("MYSQL_USER", getenv('MYSQL_USER'));
+  define("MYSQL_PASSWORD", getenv('MYSQL_PASSWORD'));
+  define("MYSQL_DATABASE", getenv('MYSQL_DATABASE'));
+  define("MYSQL_HOST", getenv('MYSQL_HOST'));
+  define("MYSQL_PORT", getenv('MYSQL_PORT'));
 
   require_once(BASE_URI."core/lib/DB.class.php");
 ?>
